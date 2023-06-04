a29b3727fc15b4c1157883835f1ed4914020f0df
Je Ar Becerro <jearbecerro@gmail.com>
2023-06-04 12:05:27 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Switch, Route, Redirect } from "react-router-dom";
-import Tables from "./pages/Tables";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
-import { useLocation } from "react-router-dom";
 import OCR from "./pages/OCR";
-import Test from "./pages/text";
 
 function App() {
   const location = useLocation()
@@ -17,8 +14,7 @@ function App() {
         <Route path="/sign-in" exact component={SignIn} />
         <Main>
           <Route exact path={["/OCR", "/"]} component={()=>{ return <OCR /> }} />
-          <Route exact path={"/test"} component={()=>{ return <Test /> }} />
-          <Route exact path="/reports" component={Tables} />
+          <Route exact path="/reports" component={()=>{ return <>On Development</>}} />
           <Redirect from="*" to={location.pathname}/>
         </Main>
       </Switch>
